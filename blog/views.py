5414845e8835134abebc937a2b43e3f0ed7dc815
vvshabrat <vvshabrat@gmail.com>
2018-04-15 20:33:44 +0300

    администрирование

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.utils import timezone   # +2)
 from .models import Post    # +1)
 from django.shortcuts import render, get_object_or_404  # +5)
+from .forms import PostForm # +7)
+
 
 # Create your views here.
 
@@ -13,3 +15,20 @@ def post_list(request):
 def post_detail(request, pk):   # +6)
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+# def post_new(request): # +8)
+#    form = PostForm()
+#    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_new(request): # +9)
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
