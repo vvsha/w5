4d09a454a061accc6e935395c2d14ce27595d311
vvshabrat <vvshabrat@gmail.com>
2018-04-15 16:31:00 +0300

    администрирование

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone   # +2)
+from .models import Post    # +1)
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')  # +3)
+#///    return render(request, 'blog/post_list.html', {})
+    return render(request, 'blog/post_list.html', {'posts': posts}) # +4)
